50f6e0605445e6c54ed944242c27941c73183513
ridafarrukh <ridafarrukh92@gmail.com>
2021-11-25 19:06:23 +0100

    remove the node

--- teau-hotel-application/backend-tests/cypress/support/commands.js
+++ teau-hotel-application/backend-tests/cypress/support/commands.js
@@ -23,3 +23,25 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+const Login_URL = 'http://localhost:3000/api/login' 
+
+Cypress.Commands.add('authenticateSession',() => {
+    const userCredentials = {
+        "username":"tester01",
+        "password":"GteteqbQQgSr88SwNExUQv2ydb7xuf8c"
+    }
+    cy.request({
+     method:"POST",
+     url:Login_URL,
+     headers:{
+        'Content-Type': 'application/json'
+     },
+     body: JSON.stringify(userCredentials)
+    }).then((response => {
+        expect(response.status).to.eq(200)
+        //cy.log(response.body)
+        Cypress.env({LoginToken:response.body})
+    }))
+    
+})
